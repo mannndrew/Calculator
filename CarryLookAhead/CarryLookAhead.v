889a23f55aac0d5b59eebfa0428c519202a7887f
Andrew Howard <mannndrew@gmail.com>
2023-03-04 01:38:55 -0600

    Finally

--- CarryLookAhead/CarryLookAhead.v
+++ CarryLookAhead/CarryLookAhead.v
@@ -1,10 +1,9 @@
-module CarryLookAhead #(parameter N = 4)
+module CarryLookAhead #(parameter N = 8)
 (
 	input [N-1:0] A,
 	input [N-1:0] B,
 	input cin,
-	output [N-1:0] Sum,
-	output test
+	output [N-1:0] Sum
 );
 
 wire [N-2:0] p;
@@ -17,17 +16,17 @@ assign
 assign
 	g[N-2:0] = (A[N-2:0] & B[N-2:0]);
 	
-
-	
-assign c[0] = cin;
-	
 //assign
 //	c[1] = g[0] | (p[0] & cin),
 //	c[2] = g[1] | (p[1] & g[0]) | (p[1] & p[0] & cin),
 //	c[3] = g[2] | (p[2] & g[1]) | (p[2] & p[1] & g[0]) | (p[2] & p[1] & p[0] & cin);
+// c[4] = g[3] | (p[3] & g[2]) | (p[3] & p[2] & g[1]) | (p[3] & p[2] & p[1] & g[0]) | (p[3] & p[2] & p[1] & p[0] & cin);
 
-wire [63:0]test1;
-wire [63:0]test2;
+// 0 - 2 terms
+// 1 - 3 terms
+// 2 - 4 terms
+
+assign c[0] = cin;
 
 genvar i, j;
 generate
@@ -35,30 +34,38 @@ generate
 
 for (i = 1; i < N; i = i + 1) begin : outer
 
+wire [N-1:0] term;
 
-assign test1[i] = p[i-1];
 
-
-//assign c[i] = g[i-1] | &p[i-1:0] & cin;							//WORK IN PROGRESS
-
-//	for (j = 0; j < N; j = j + 1) begin : inner
-//		
-//		test[j]
-//		
-//	end
+	for (j = 0; j < i+1; j = j + 1) begin : inner			// First Term
+		if (j == 0) begin
+			assign term[j] = g[i-1];
+		end
+		
+		else if (j == i) begin
+			assign term[j] = &p[i-1:0] & cin;		// Last Term
+		end
+		
+		else begin
+			assign term[j] = &p[i-1:i-j] & g[i-j-1];			// Middle Terms
+		end
+		
+	end
+	
+	assign c[i] = |term[i:0];										// Adds terms together
+	
 
 end
 
-
 endgenerate
 
-assign test = |test1[3:0];
+
 //FullAdder S0(.a(A[0]), .b(B[0]), .cin(c[0]), .sum(Sum[0]));
 //FullAdder S1(.a(A[1]), .b(B[1]), .cin(c[1]), .sum(Sum[1]));
 //FullAdder S2(.a(A[2]), .b(B[2]), .cin(c[2]), .sum(Sum[2]));
 //FullAdder S3(.a(A[3]), .b(B[3]), .cin(c[3]), .sum(Sum[3]));
 
-//FullAdder S[N-1:0](.a(A[N-1:0]), .b(B[N-1:0]), .cin(c[N-1:0]), .sum(Sum[N-1:0]));
+FullAdder S[N-1:0](.a(A[N-1:0]), .b(B[N-1:0]), .cin(c[N-1:0]), .sum(Sum[N-1:0]));
 
 
 endmodule
