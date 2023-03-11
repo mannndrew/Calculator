3f6da9b6cb9c60f957aad5143be4bf1c2b27457a
Andrew Howard <mannndrew@gmail.com>
2023-03-11 16:46:29 -0600

    Updated -0 condition

--- Calculator/InputUnit/BCD2BinarySM.v
+++ Calculator/InputUnit/BCD2BinarySM.v
@@ -6,19 +6,21 @@ module BCD2BinarySM
 
 always @(BCD)
 
-if (BCD[15:12] == 4'b1110 && BCD[11:8] == 4'b0001 && BCD[7:4] == 4'b0010 && BCD[3:0] == 4'b1000)
+if (BCD[15:12] == 4'b1110 && BCD[11:8] == 4'b0001 && BCD[7:4] == 4'b0010 && BCD[3:0] == 4'b1000)	//Special Case: -128
 	binarySM = 8'b10000000;
 	
-else if (BCD[15:12] == 4'b1110)
-	binarySM = 8'b10000000 + BCD[11:8]*(8'b01100100) + BCD[7:4]*(8'b1010) + BCD[3:0];
+else if (BCD[15:12] == 4'b1110 && BCD[11:0] != 12'd0)
+	binarySM = 8'b10000000 + BCD[11:8]*(8'b01100100) + BCD[7:4]*(8'b1010) + BCD[3:0];					
 
-else if (BCD[11:8] == 4'b1110)
+else if (BCD[11:8] == 4'b1110 && BCD[7:0] != 8'd0)
 	binarySM = 8'b10000000 + BCD[7:4]*(8'b1010) + BCD[3:0];
 	
-else if (BCD[7:4] == 4'b1110)
+else if (BCD[7:4] == 4'b1110 && BCD[3:0] != 4'd0)
 	binarySM = 8'b10000000 + BCD[3:0];
 else if (BCD[3:0] == 4'b1110)
 	binarySM = 8'b00000000;
-else
+else if (BCD[15:12] != 4'b1110 && BCD[11:8] != 4'b1110 && BCD[7:4] != 4'b1110 && BCD[3:0] != 4'b1110)
 	binarySM = BCD[11:8]*(8'b01100100) + BCD[7:4]*(8'b1010) + BCD[3:0];
+else
+	binarySM = 8'b00000000;																									// -0
 endmodule
